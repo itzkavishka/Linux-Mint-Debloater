f12643a409a0a87068859b79f68c312c8311c98e
ikz <kavishkadahanayaka@gmail.com>
2024-09-26 17:57:04 +0530

    add more unwanted applications to the terminal list

--- debloat-mint.sh
+++ debloat-mint.sh
@@ -9,9 +9,6 @@ if [ $? = 0 ]; then
 	sudo apt purge libreoffice-core -y
 	sudo apt purge libreoffice-common -y
 	sudo apt purge transmission-gtk -y
-	sudo apt purge hexchat -y
-	sudo apt purge baobab -y
-	sudo apt purge seahorse -y
 	sudo apt purge thunderbird -y
 	sudo apt purge rhythmbox -y
 	sudo apt purge pix -y
@@ -21,11 +18,21 @@ if [ $? = 0 ]; then
 	sudo apt purge xreader -y
 	sudo apt purge onboard -y
 	sudo apt purge gnome-calendar -y
-z	sudo apt purge celluloid -y
 	sudo apt purge gnome-logs -y
 	sudo apt purge gnome-power-manager -y
 	sudo apt purge onboard -y
 	sudo apt purge warpinator -y
+    sudo apt purge xfce4-dict -y
+    sudo apt purge sticky -y
+    sudo apt purge mintwelcome* -y
+    sudo apt purge gnome-calculator -y
+    sudo apt purge hypnotix -y
+    sudo apt purge blueman* -y
+    sudo apt purge system-config-printer -y
+    sudo apt purge gucharmap -y
+
+    # Remove WebApps
+    rm  $HOME/.local/share/applications/*
 
 	sudo apt autoremove -y && sudo apt clean
 else
